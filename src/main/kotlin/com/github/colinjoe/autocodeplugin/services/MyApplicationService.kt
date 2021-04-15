6835a45014ce2bcaf854c01cfc32aecf80d3a81c
GitHub Action <action@github.com>
2021-04-15 11:40:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/colinjoe/autocodeplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/colinjoe/autocodeplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.colinjoe.autocodeplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.colinjoe.autocodeplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/colinjoe/autocodeplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/colinjoe/autocodeplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.colinjoe.autocodeplugin.services
 
+import com.github.colinjoe.autocodeplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
